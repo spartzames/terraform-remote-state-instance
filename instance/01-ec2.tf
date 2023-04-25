2c4f885064e7dc86357fe8ad137e55b539eb9ae4
spartzames <spartzames@gmail.com>
2023-04-25 14:19:09 +0900

    fix

--- instance/01-ec2.tf
+++ instance/01-ec2.tf
@@ -19,23 +19,10 @@ resource "aws_security_group" "resource-tagging-lab-kthong" {
   vpc_id = data.terraform_remote_state.remote_state.outputs.output_vpc_kthong.id
 }
 
-# Terraform >= 0.12
-# resource "aws_instance" "foo" {
-#   # ...
-#   subnet_id = data.terraform_remote_state.remote_state.outputs.subnet_id
-# }
-
-# Terraform <= 0.11
-# resource "aws_instance" "foo" {
-#   # ...
-#   subnet_id = "${data.terraform_remote_state.remote_state.subnet_id}"
-# }
-
-
 resource "aws_instance" "ec2_kthong" {
   ami                    = "ami-0d9f286195031c3d9"
   instance_type          = "t2.micro"
-  subnet_id              = data.terraform_remote_state.remote_state.outputs.aws_subnet.public-ap-southeast-2a.id
+  subnet_id              = data.terraform_remote_state.remote_state.outputs.output_aws_subnet_public_2a_kthong.id
   vpc_security_group_ids = [aws_security_group.resource-tagging-lab-kthong.id]
 }
 
